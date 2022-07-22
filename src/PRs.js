c3311ee0e8157ad8bd0ddc0fbdcb9e4198eed003
Sanjana Navarathne <62108894+Sanjana-99@users.noreply.github.com>
2022-07-22 12:13:12 +0530

    Initial commit to PRList (#2)

---
+++ src/PRs.js
@@ -0,0 +1,32 @@
+export const PRs = [
+    {
+        id: 1,
+        Name: 'Sanjana Navarathne',
+        PRDate: '16/7/2022',
+        Issue: 'Issue 1'
+    },
+    {
+        id: 2,
+        Name: 'Yaiya Sam',
+        PRDate: '16/7/2022',
+        Issue: 'Issue 2'
+    },
+    {
+        id: 3,
+        Name: 'Heshan Andrews',
+        PRDate: '16/7/2022',
+        Issue: 'Issue 3'
+    },
+    {
+        id: 4,
+        Name: 'Pasindu Ranasinghe',
+        PRDate: '16/7/2022',
+        Issue: 'Issue 4'
+    },
+    {
+        id: 5,
+        Name: 'Kaveesha Perera',
+        PRDate: '16/7/2022',
+        Issue: 'Issue 5'
+    }
+]
